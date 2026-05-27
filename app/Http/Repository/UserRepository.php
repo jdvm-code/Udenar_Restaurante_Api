3cadaa809f9d2d742fd8a14a0278fbc8bf2f6f73
jdvm-code <davidjmvelasco@gmail.com>
2026-05-27 13:47:10 -0500

    Implementamos JWTsubject en modelo User

--- app/Http/Repository/UserRepository.php
+++ app/Http/Repository/UserRepository.php
@@ -1,38 +1,35 @@
 <?php
+
 namespace App\Http\Repository;
 
 use App\Http\Services\UserService;
+use App\Mail\ConfirmAccount;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UserRepository extends BaseRepository implements UserService
 {
     public function __construct(private User $model)
     {
         parent::__construct($model);
-    
     }
-      public function store(Request $request)
+    public function store(Request $request)
     {
-        try {
-            $token =bin2hex(random_bytes(16));
-            $user = User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => Hash::make($request->password),
-                'remeber_token'=> $token,
-                'role_id'=>$request->role_id
-            ]);
-            // Generate token
-            //$token = JWTAuth::fromUser($user);
-            //Mail::to($user->email)->send(new ConfirmAccount($user));
-            return $user;
-        } catch (\Exception $e) {
-            // Handle the exception (e.g., log the error, return an error response) 
-            return response()->json(['error' => 'Failed to create user', 'message' => $e->getMessage()], 500);
-        }
+        $token = bin2hex(random_bytes(16));
+
+        // Si esto falla (por ejemplo, correo duplicado), 
+        // Laravel lanzará una excepción automáticamente hacia el controlador.
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'remember_token' => $token,
+            'role_id' => $request->role_id
+        ]);
+
+        return $user;
     }
-
-
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,15 +9,34 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
+    // ... (Tus propiedades fillable, hidden, etc. se quedan tal como están)
+
+    /**
+     * 3. AGREGA ESTE MÉTODO OBLIGATORIO
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * 4. AGREGA ESTE MÉTODO OBLIGATORIO
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    /** @use HasFactory<UserFactory> */
     /**
      * Get the attributes that should be cast.
      *
