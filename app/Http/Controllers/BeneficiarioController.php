87d8a41d62b79e61a5cfee26bc9f8060d2537dae
jdvm-code <davidjmvelasco@gmail.com>
2026-05-26 19:53:47 -0500

    Estructura inicial del proyecto RestaUdenarApi

---
+++ app/Http/Controllers/BeneficiarioController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Beneficiario;
+use Illuminate\Http\Request;
+
+class BeneficiarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Beneficiario $beneficiario)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Beneficiario $beneficiario)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Beneficiario $beneficiario)
+    {
+        //
+    }
+}
